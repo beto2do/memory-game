a4914f5764a8690f73e7a873dd61da96f0d7d017
beto2do <humberto2do@gmail.com>
2020-09-27 13:09:44 -0700

    added dynamic generation of card objects

--- src/memory.game.js
+++ src/memory.game.js
@@ -1,31 +1,58 @@
 import React from 'react';
-import Grid from './components/grid'
+import Grid from './components/grid';
+import { v4 as uuidv4 } from 'uuid';
 
-const cards = [
-    {
-        id: "1",
+function getImages() {
+    const images = [
+        'https://cdn.pixabay.com/photo/2015/11/17/13/13/bulldog-1047518_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2015/10/25/14/43/bucket-1005891_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2018/10/05/21/21/spider-3726881_960_720.png',
+        'https://cdn.pixabay.com/photo/2013/07/07/13/49/train-143847_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2018/02/26/21/04/owl-3184032_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2016/11/18/13/47/apple-1834639_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2015/03/13/12/03/lego-671593_960_720.jpg',
+        'https://cdn.pixabay.com/photo/2017/01/30/17/37/balloon-2021525_960_720.jpg',
+    ];
+
+    //TODO it should have a real request.
+    return new Promise((resolve, reject) => {
+        setTimeout(function(){ resolve(images); }, 100);
+    });
+}
+
+function createCard(imgUrl) {
+    const imgBackSrc = "https://cdn.pixabay.com/photo/2017/06/10/07/15/joystick-2389216_960_720.png";
+    return {
+        id: uuidv4(),
         isToggleOn: true,
-        imgFrontSrc: "https://cdn.pixabay.com/photo/2018/10/05/21/21/spider-3726881_960_720.png",
-        imgBackSrc: "https://cdn.pixabay.com/photo/2017/06/10/07/15/joystick-2389216_960_720.png"
-    },
-    {
-        id: "2",
-        isToggleOn: true,
-        imgFrontSrc: "https://cdn.pixabay.com/photo/2018/10/05/21/21/spider-3726881_960_720.png",
-        imgBackSrc: "https://cdn.pixabay.com/photo/2017/06/10/07/15/joystick-2389216_960_720.png"
-    },
-    {
-        id: "3",
-        isToggleOn: false,
-        imgFrontSrc: "https://cdn.pixabay.com/photo/2018/10/05/21/21/spider-3726881_960_720.png",
-        imgBackSrc: "https://cdn.pixabay.com/photo/2017/06/10/07/15/joystick-2389216_960_720.png"
+        imgFrontSrc:  imgUrl,
+        imgBackSrc: imgBackSrc
     }
-];
+}
 
-function MemoryGame(props) {
-    return (
-        <Grid cards={cards}/>
-    );
+function createDataCards() {
+    const images = getImages();
+    return images.then(imgList => imgList.map(imgUrl => createCard(imgUrl)));
+}
+
+class MemoryGame extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            cards: []
+        }
+    }
+
+    componentDidMount() {
+        createDataCards().then(cards => this.setState({cards: cards}));
+    }
+    
+    render() {
+        return (
+            <Grid cards={this.state.cards}/>
+        );
+    }
 }
 
 export default MemoryGame;
